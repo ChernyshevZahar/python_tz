295e13acd865d5f75f2ebbcd1f56fbccdcedb601
ChernyshevZahar <chernyshevzahar@gmail.com>
2024-10-08 17:26:38 +0300

    сделал первое дз(остальные задачи)

---
+++ ls_1/program_3.py
@@ -0,0 +1,20 @@
+
+n = int(input('Введите количество чисел в последовательности: '))
+
+
+count = 0
+
+for i in range(n):
+
+    number = int(input('Введите очередное число: '))
+   
+    if number > 1: 
+        for divider in range(2, number):
+        
+            if number % divider == 0:
+                break
+            else:
+            
+                count += 1
+            
+            print('Количество простых чисел:', count)
